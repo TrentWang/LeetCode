e12266227582c40f20f39c64603f19d94b21a2bf
TrentWang <abc779595@gmail.com>
2024-02-11 22:40:20 +0800

    Update KMP.cpp

--- String/KMP.cpp
+++ String/KMP.cpp
@@ -1,4 +1,35 @@
-// https://yeefun.github.io/kmp-algorithm-for-beginners/
+// https://github.com/wisdompeak/LeetCode/blob/master/String/028.Implement-strStr/028.Implement-strStr-KMP.cpp
+int strStr(string s, string p) {
+    int n = s.size();
+    int m = p.size();
+    if (m == 0) return 0;
+    if (n == 0) return -1;    
+
+    vector<int> suf = longestCommonPrefixSuffix(p);
+        
+    vector<int> dp(n, 0); // dp[i]: the maximum length K s.t. p[0:K-1] == s[i-K+1:i]
+    dp[0] = (s[0] == p[0]);
+    if (m == 1 && dp[0] == 1)
+        return 0;
+
+    for (int i = 1; i < n; i++) {
+        int j = dp[i-1];
+        while (j > 0 && (j == m || s[i] != p[j]))
+            j = suf[j-1];
+
+        if(j == 0)
+            dp[i] = (s[i] == p[0]);
+        else
+            dp[i] = j + 1;
+
+        if (dp[i] == m)
+            return i - m + 1;
+    }
+    return -1;
+}
+
+
+//////// https://yeefun.github.io/kmp-algorithm-for-beginners/ 
 // s: main string
 // p: pattern string
 // return: index of first occurrence of p in s, -1 if not found
